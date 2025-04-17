b96f2e778bbeea0b835e18a2b93c72680147b502
sevinc995 <sevincseferova25@gmail.com>
2025-04-17 18:42:14 +0000

    link

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .config import API_KEY
 app = Flask(__name__)
 
 CORS(app, resources = {r"/api/news": {
-    "origins": [],
+    "origins": ["https://sevinc995.github.io"],
     "methods": ["GET"]
 }})
 
